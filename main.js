f54fa7f6a7a8f071744651bbeb9d90b864e4f569
defens53 <defens5533@gmail.com>
2025-03-18 20:21:19 +0300

    первый коммит

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
+
+
 let arr = [2, 5, 9, 15, 0, 4];
 
-for (let num of arr) {
-    if (num > 3 && num < 10) {
-        console.log(num);
+
+for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] > 3, arr[i] < 10) {
+        console.log(arr[i]);
     }
 }
 
 
-let numbers = [2, -5, 9, -15, 0, 4, -3, 8];
 
-let sumPositive = numbers.reduce((sum, num) => num > 0 ? sum + num : sum, 0);
+let arr2 = [-11, -54, 5, 32, -77, 34];
+let num = 0
 
-console.log("Сумма положительных элементов:", sumPositive);
+for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] > 0) {
+        num += arr2[i]
+    }
+}
+console.log(num);
+
+
